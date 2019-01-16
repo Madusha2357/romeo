c17befe988ed63ccfedc13028c3228f39ad8495b
Madusha <madusha@Randikas-MacBook-Pro.local>
2019-01-16 20:43:33 +0530

    first commit

---
+++ src/test/java/com/madusha/spring/romeo/RomeoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.madusha.spring.romeo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RomeoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
